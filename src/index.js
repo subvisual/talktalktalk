ca4d0059d3ae334e038ca48f3695866fd9a29cb4
Bruno Azevedo <azevedo.252@gmail.com>
2017-04-21 15:49:45 +0100

    Recognizes speech

--- src/index.js
+++ src/index.js
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
 
+window.SpeechRecognition = window.SpeechRecognition ||
+                           window.webkitSpeechRecognition ||
+                           null;
+
 ReactDOM.render(
   <App />,
   document.getElementById('root')
